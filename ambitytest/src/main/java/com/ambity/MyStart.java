f421059ab6235f9864eb4ee3271c1f1c150b1941
ambity <2431396974@qq.com>
2020-10-12 22:06:21 +0800

    first commit

---
+++ ambitytest/src/main/java/com/ambity/MyStart.java
@@ -0,0 +1,16 @@
+package com.ambity;
+
+import com.ambity.service.MyService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ambity.service")
+public class MyStart {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyStart.class);
+		MyService myService = context.getBean(MyService.class);
+		myService.say();
+	}
+}
---
+++ ambitytest/src/main/java/com/ambity/service/MyService.java
@@ -0,0 +1,10 @@
+package com.ambity.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+	public void say(){
+		System.out.println("aaaa");
+	}
+}
